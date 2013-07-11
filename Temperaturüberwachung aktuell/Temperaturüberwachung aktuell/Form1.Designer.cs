041931974ac517fee159a51b17ec820c36f7e655
zensiert <blase.florian@googlemail.com>
2013-07-11 12:25:46 +0200

    first commit

---
+++ Temperaturüberwachung aktuell/Temperaturüberwachung aktuell/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(21, 54);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(163, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ports";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(163, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Sensor 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(166, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sensor 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Temperaturüberwachung";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Temperaturüberwachung aktuell/Temperaturüberwachung aktuell/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public delegate void AddDataDelegate(String myString);
+        public delegate void AddDataDelegate2(String myString2);
+        public AddDataDelegate myDelegate;
+        public AddDataDelegate2 myDelegate2;
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == false)
+            {
+                serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                serialPort1.Open();
+                button1.Text = "disconnect";
+            }
+            else
+            {
+                serialPort1.Close();
+                button1.Text = "connect";
+            }
+               
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.DataSource = SerialPort.GetPortNames();
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(serialPort1_DataReceived);
+            this.myDelegate = new AddDataDelegate(AddDataMethod);
+            this.myDelegate2 = new AddDataDelegate2(AddDataMethod2);
+            System.IO.StreamWriter file = new System.IO.StreamWriter("C:\\Users\\Florian\\Test\\Temperaturauswertung.txt", true);
+            file.WriteLine("Datum" + "      " + "Uhrzeit" + "   " + "Sensor1" + "  " + "Sensor2");
+            file.Close();
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {           
+            SerialPort serialPort1 = (SerialPort)sender;
+            string alles = serialPort1.ReadExisting();
+            string[] allessplit = alles.Split(new Char [] { ' ' });
+            int count = 1;
+            foreach (string einzel in allessplit)
+            {
+                System.Threading.Thread.Sleep(500);
+                string filter = einzel;
+                if (filter.Length == 5)
+                {                    
+                     if (count == 1)
+                     {
+                         textBox1.Invoke(this.myDelegate, new Object[] { filter });
+                         count = 2;
+                     }
+                     else
+                     {
+                         textBox2.Invoke(this.myDelegate2, new Object[] { filter });
+                         count = 1;
+                     }
+                 }
+            }
+
+            System.IO.StreamWriter file = new System.IO.StreamWriter("C:\\Users\\Florian\\Test\\Temperaturauswertung.txt",true);
+            //file.WriteLine("Sensor1"+"  " + "Sensor2");
+            file.WriteLine(DateTime.Now +"  " + textBox1.Text + "    " + textBox2.Text);
+            file.Close();
+         }
+
+        private void displayTime()
+        {
+          DateTime.Now.ToShortTimeString();
+        }
+            
+         public void AddDataMethod(String myString)
+        {
+            textBox1.Text = myString;
+        }
+
+         public void AddDataMethod2(String myString2)
+         {         
+             textBox2.Text = myString2;
+         }   
+    }
+}
